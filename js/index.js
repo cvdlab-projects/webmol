d9d2a5d41fea3319540a29c42e14dc8d93a6ddd9
Antonio Gallo <dokkis@hotmail.it>
2012-09-24 21:23:35 +0200

    cambiato site

--- js/index.js
+++ js/index.js
@@ -51,7 +51,7 @@ function ProcessRequest()
 
   // Funzion per scaricare le proteine, cambiare la variabile site per puntare ad altri repository
   function downloadProtein(){
-    var site = "https://raw.github.com/cvdlab-bio/webmol/master/proteine/";
+    var site = "http://cvdlab-bio.github.com/webmol/proteine/";
     var url = site + document.getElementById('idProtein').value + ".json";
     httpGet(url);
   }
